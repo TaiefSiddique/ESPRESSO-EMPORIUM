4773db35dbf4bd20ef00c0bcf20aebe4c9e844ef
TaiefSiddique <taiefsiddik2@gmail.com>
2023-10-27 11:33:59 +0600

    images added

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.send('Coffee making server is running')
+})
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000');
-});
+app.listen(port, () => {
+    console.log(`Coffee Server is running on port: ${port}`)
+})
