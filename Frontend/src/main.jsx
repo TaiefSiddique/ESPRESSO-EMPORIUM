da71db2349d861cdad4b5806728cc2103a7f7a5f
TaiefSiddique <taiefsiddik2@gmail.com>
2023-10-29 23:13:12 +0600

    User viewing table added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -11,6 +11,7 @@ import UpdateCoffee from './components/UpdateCoffee.jsx';
 import SignUp from './components/SignUp.jsx';
 import SignIn from './components/SignIn.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import Users from './components/Users.jsx';
 
 
 const router = createBrowserRouter([
@@ -36,6 +37,11 @@ const router = createBrowserRouter([
     path: '/signin',
     element: <SignIn></SignIn>
   },
+  {
+    path: '/users',
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5002/user')
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
